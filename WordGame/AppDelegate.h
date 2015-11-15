ee82d1e746cee0171eaa00af08b2456d6cef2a34
Gideon <Gideon@panxiaotaodeMacBook-Pro.local>
2015-11-15 16:12:53 +0800

    初始化

---
+++ WordGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordGame
+//
+//  Created by Gideon on 15/11/15.
+//  Copyright (c) 2015年 Gideon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordGame
+//
+//  Created by Gideon on 15/11/15.
+//  Copyright (c) 2015年 Gideon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
